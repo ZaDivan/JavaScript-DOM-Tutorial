fbbd461e9d8dee27942612067265d9ca4dca5334
ZaDivan <78017590+ZaDivan@users.noreply.github.com>
2021-03-16 14:03:11 +0200

    Delete app10.js

--- app10.js
+++
@@ -1,9 +0,0 @@
-const list = document.querySelector('#book-list ul');
-
-// delete books
-list.addEventListener('click', (e) => {
-  if(e.target.className == 'delete'){
-    const li = e.target.parentElement;
-    li.parentNode.removeChild(li);
-  }
-});
